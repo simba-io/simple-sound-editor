781f2e6c69dd67967ccf45f654b1616150bedc1c
Cameron McKay <camerondeveloper93@gmail.com>
2026-02-22 16:43:58 +0000

    fix: uncaught type error

--- src/SoundManager.ts
+++ src/SoundManager.ts
@@ -2,9 +2,9 @@ import { Signal } from "tone";
 import {SoundEditor, UserPanelAnchor} from "./EditorView";
 import * as Tone from "tone";
 
-interface SoundManagerOptions extends AudioParam{
+/* interface SoundManagerOptions extends AudioParam{
     dispatchSoundManager: () => void;
-}
+} */
 /** @internal */
 export class SoundManager
 {
@@ -28,7 +28,7 @@ export class SoundManager
             this.editor = new SoundEditor(this.anchor);
         }
 
-        let options!: SoundManagerOptions;
+        /* let options!: SoundManagerOptions;
 
         const dispatchSoundManager = () => {
             console.log("SoundManager DISPATCHED");
@@ -39,7 +39,7 @@ export class SoundManager
         this.signal = new Signal();
         this.signal.apply(options);
 
-        this.signal.dispose();
+        this.signal.dispose(); */
 
         console.log("SoundManager initialized");
     }
